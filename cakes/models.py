75bcec8e2181cb21426a22269e1ae32014873ea7
David Schnoll <davidschnoll@gmail.com>
2024-02-14 21:38:28 +0200

    Create db models and respective admins

--- cakes/models.py
+++ cakes/models.py
@@ -1,3 +1,134 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Level(models.Model):
+    amount = models.IntegerField(verbose_name="количество уровней")
+    price = models.IntegerField(verbose_name="стоимость", validators=[MinValueValidator(0)])
+
+    class Meta:
+        verbose_name = "уровень"
+        verbose_name_plural = "уровни"
+
+    def __str__(self):
+        return str(self.amount)
+
+
+class Shape(models.Model):
+    name = models.CharField(verbose_name="название", max_length=20)
+    price = models.IntegerField(verbose_name="стоимость", validators=[MinValueValidator(0)])
+
+    class Meta:
+        verbose_name = "форма"
+        verbose_name_plural = "формы"
+
+    def __str__(self):
+        return self.name
+
+
+class Topping(models.Model):
+    name = models.CharField(verbose_name="название", max_length=20)
+    price = models.IntegerField(verbose_name="стоимость", validators=[MinValueValidator(0)])
+
+    class Meta:
+        verbose_name = "топпинг"
+        verbose_name_plural = "топпинги"
+
+    def __str__(self):
+        return self.name
+
+
+class Berry(models.Model):
+    name = models.CharField(verbose_name="название", max_length=20)
+    price = models.IntegerField(verbose_name="стоимость", validators=[MinValueValidator(0)])
+
+    class Meta:
+        verbose_name = "ягода"
+        verbose_name_plural = "ягоды"
+
+    def __str__(self):
+        return self.name
+
+
+class Decoration(models.Model):
+    name = models.CharField(verbose_name="название", max_length=20)
+    price = models.IntegerField(verbose_name="стоимость", validators=[MinValueValidator(0)])
+
+    class Meta:
+        verbose_name = "украшение"
+        verbose_name_plural = "украшения"
+
+    def __str__(self):
+        return self.name
+
+
+class Cake(models.Model):
+    levels = models.ForeignKey(
+        Level,
+        verbose_name="количество уровней",
+        on_delete=models.PROTECT,
+        related_name="cakes"
+    )
+    shape = models.ForeignKey(
+        Shape,
+        verbose_name="форма торта",
+        on_delete=models.PROTECT,
+        related_name="cakes"
+    )
+    topping = models.ForeignKey(
+        Topping,
+        verbose_name="топпинг",
+        on_delete=models.PROTECT,
+        related_name="cakes"
+    )
+    berries = models.ManyToManyField(
+        Berry,
+        verbose_name="ягоды",
+        related_name="cakes"
+    )
+    decorations = models.ManyToManyField(
+        Decoration,
+        verbose_name="украшения",
+        related_name="cakes"
+    )
+    text = models.CharField(
+        verbose_name="надпись",
+        max_length=75,
+        blank=True
+    )
+    cost = models.DecimalField(
+        verbose_name="стоимость",
+        validators=[MinValueValidator(0)],
+        decimal_places=2,
+        max_digits=7
+    )
+
+    class Meta:
+        verbose_name = "торт"
+        verbose_name_plural = "торты"
+
+    def __str__(self):
+        return f"{self.levels}-слойный {self.shape}, топпинг - {self.topping}"
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, verbose_name="пользователь", on_delete=models.CASCADE, related_name="orders")
+    cake = models.ForeignKey(Cake, verbose_name="торт", on_delete=models.CASCADE, related_name="orders")
+    address = models.TextField(verbose_name="адрес", editable=True)
+    notes = models.TextField(verbose_name="примечание", blank=True)
+    delivery_date = models.DateField(verbose_name="дата доставки")
+    delivery_time = models.TimeField(verbose_name="время доставки")
+    cost = models.DecimalField(
+        verbose_name="стоимость заказа",
+        validators=[MinValueValidator(0)],
+        decimal_places=2,
+        max_digits=7
+    )
+
+    class Meta:
+        verbose_name = "заказ"
+        verbose_name_plural = "заказы"
+
+    def __str__(self):
+        return f"{self.cake} - {self.address}"
